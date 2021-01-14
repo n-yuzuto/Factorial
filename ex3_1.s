f9e3e8d90d0f0dcf23ad6836ae473d7c6c6c4fc3
no-name-git <76964063+no-name-git@users.noreply.github.com>
2021-01-15 03:48:20 +0900

    change commit
    change file

--- ex3_1.s
+++ ex3_1.s
@@ -1,17 +1,46 @@
-
-        .data
-x: .quad 20
+       .data
+x: .quad 0x0123456789abcdef
         .text
         .globl main
 main:
-mov $1, %rax
-mov $0, %rbx
-L1:
-add $1, %rbx
-cmp x, %rbx
-je L2
-mul %rbx 
-jmp L1
-L2:
-mul %rbx
+mov $0,%rax
+mov $0,%r8
+
+
+.L1:
+mov x,%rbx
+mov $0,%r9
+
+
+.L2:
+cmp %r9,%r8
+jz .L3
+shl $1,%rbx
+add $1,%r9
+jmp .L2
+
+
+.L3:
+shr $63,%rbx
+mov $0,%r9
+jmp .L4
+
+
+.L4:
+cmp %r9,%r8
+jz .L5
+shl $1,%rbx
+add $1,%r9
+jmp .L4
+
+
+.L5:
+add %rbx,%rax
+cmp $63,%r8
+jz .L6
+add $1,%r8
+jmp .L1
+
+
+.L6:
 call finish
